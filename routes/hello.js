0c20c3dc72991df96f3cb6dc3e1234944a83b618
Liu, Zhengyang <zhengyal@amazon.com>
2015-01-12 19:11:50 -0800

    version 2

--- routes/hello.js
+++ routes/hello.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 
 router.get('/', function(req, res) {
-    res.send('Hello world!');
+    res.send('Hello world!<br />I am running version 2!');
 });
 
 module.exports = router;
