16a414db6d7f6a467386b676544bcfcf5446b1d5
Liu, Zhengyang <zhengyal@amazon.com>
2015-04-16 12:21:56 -0700

    Adding check for request IP range

--- routes/update.js
+++ routes/update.js
@@ -1,6 +1,9 @@
 var router = require('express').Router(),
     AWS = require('aws-sdk'),
-    properties = require('properties');
+    properties = require('properties'),
+    ip = require('ip'),
+    bodyParser = require('body-parser'),
+    request = require('request');
 
 var selfEnvironmentId = null,
 	eb = null;
@@ -17,12 +20,28 @@ properties.parse('/etc/elasticbeanstalk/.aws-eb-stack.properties', { path: true 
     selfEnvironmentId = data.environment_id;
 });
 
+router.use(bodyParser.json());
+
 router.post('/', function(req, res) {
+	// checks whether the request came from Docker Hub servers
+	// X-Forwarded-For can be spoofed! Assigning an obscure URL for the update hook is still recommended
+	var reqIp = req.headers['x-forwarded-for'] ?
+				req.headers['x-forwarded-for'].split(',')[0] :        // ELB(w/ or w/o nginx) / Single instance(w/ nginx)
+				req.connection.remoteAddress.replace('::ffff:', '');  // Single instance(w/o nginx)
+
+	if (!(ip.toLong(reqIp) >= ip.toLong('162.242.195.64') && ip.toLong(reqIp) <= ip.toLong('162.242.195.127'))) {
+		res.status(403);
+		res.send('');
+		return;
+	}
+
 	if (!eb || !selfEnvironmentId) {
 		res.status(500);
 		res.send('');
 		return;
 	}
+
+	var callbackUrl = req.body.callback_url;
 
     eb.describeEnvironments(
     	{ 
@@ -46,10 +65,24 @@ router.post('/', function(req, res) {
 		                console.log(err);
 		                res.status(500);
 		                res.send('');
+		                request.post({
+			            	url: callbackUrl,
+			            	json: true,
+			            	body: {
+			            		state: 'error'
+			            	}
+			            }, function() {});
 		                return;
 		            }
 
-		            res.send(JSON.stringify(data));
+		            res.send('');
+		            request.post({
+		            	url: callbackUrl,
+		            	json: true,
+		            	body: {
+		            		state: 'success'
+		            	}
+		            }, function() {});
 	        	}
 	        );
     	}
